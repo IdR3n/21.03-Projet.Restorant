dbbeab35554657bd9324c2a274e43c8be06c78cc
Idris B.SARIKAYA <bidrisarikaya@gmail.com>
2024-03-21 13:04:02 +0100

    ...

---
+++ projet-restorant/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Reservation from './Compenents/Reservation.jsx';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Reservation></Reservation>
+      </header>
+    </div>
+  );
+}
+
+export default App;
